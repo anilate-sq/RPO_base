b6b0da43df6c8cbc37070338a9a55f9e534abfbd
Andrew <03051820dr@gmail.com>
2026-02-03 10:52:15 +0800

    update lesson_26_Write.cpp

--- МДК.01.05 Основы программирования на языке С++/Пары/26. Работа с файлами (fstream)/lesson_026_Write.cpp
+++ МДК.01.05 Основы программирования на языке С++/Пары/26. Работа с файлами (fstream)/lesson_026_Write.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main(){
-        // Поток для чтения
-        ofstream fout("data.txt"); // Считывание данных из файлов
+        // Поток для записи
+        ofstream fout("data.txt"); // Запись данных из файлов
         fout << "Здарова дурак\n";
         fout << 12 << " / " << 21; 
 }

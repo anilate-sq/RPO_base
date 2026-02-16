c5b1af0af0b7b1827a6c5852715413e2b259a9b7
anilate-sq <andreykontsevich@yandex.ru>
2026-02-16 20:08:46 +0800

    Добавил пару 21

--- МДК.01.01 Разработка программных модулей/Пары/21. Data Access Layer/ex_003_Repository/db/db.py
+++ МДК.01.01 Разработка программных модулей/Пары/21. Data Access Layer/ex_003_Repository/db/db.py
@@ -2,4 +2,4 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def get_conn():
-    return create_engine('postgresql+psycopg2://postgres:EndryuConverse141719@localhost:5432/rpo_db')
+    return create_engine('postgresql+psycopg2://postgres:123@localhost:5432/rpo_db')
